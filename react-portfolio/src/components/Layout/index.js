70304ade630828f6cd3849fd5bc3919cbbea554c
slahel <74063357+slahel@users.noreply.github.com>
2022-11-10 16:57:45 +0000

    footer css

--- react-portfolio/src/components/Layout/index.js
+++ react-portfolio/src/components/Layout/index.js
@@ -2,6 +2,8 @@ import "./index.scss";
 import Sidebar from "../Sidebar";
 import { Outlet } from "react-router";
 //import logo from "../../images/Vector.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEnvelope, faGear } from "@fortawesome/free-solid-svg-icons";
 
 export default function Layout() {
   return (
@@ -9,6 +11,17 @@ export default function Layout() {
       <Sidebar />
       <Outlet />
       {/* <img src={logo} alt="logo" className="logo-faded" /> */}
+      <footer>
+        {/* <a href="/myworks" className="footer-as">
+          My Works
+        </a>
+        <a to="/skills" className="footer-as">
+          My Skills
+        </a>
+        <a to="/contact" className="footer-as">
+          Contact Me
+        </a> */}
+      </footer>
     </div>
   );
 }

9f1c37966d0396bc4b099631f08b8c8643b8537f
slahel <74063357+slahel@users.noreply.github.com>
2022-11-10 12:23:18 +0000

    function test

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -15,16 +15,17 @@ import { useState } from "react";
 
 export default function Sidebar() {
   const [showMenu, setShowMenu] = useState(false);
-  return (
-    // function myFunction() {
-    //   var x = document.getElementById("myLinks");
-    //   if (x.style.display === "block") {
-    //     x.style.display = "none";
-    //   } else {
-    //     x.style.display = "block";
-    //   }
-    // }
 
+  //   /* Open when someone clicks on the span element */
+  // function openNav() {
+  //   document.getElementById("myNav").style.width = "100%";
+  // }
+
+  // /* Close when someone clicks on the "x" symbol inside the overlay */
+  // function closeNav() {
+  //   document.getElementById("myNav").style.width = "0%";
+  // }
+  return (
     <div className="nav-bar">
       <Link className="logo" to="/">
         <img src={Logo} alt="logo" />
